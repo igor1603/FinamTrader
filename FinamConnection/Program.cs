2a9ac396fa0397ac7b1a2bd9b731f7732a331c74
igor1603 <igmi@mail.ru>
2026-04-27 10:50:08 +0300

    Initial project structure

---
+++ FinamConnection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FinamConnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
